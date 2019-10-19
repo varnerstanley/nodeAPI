4732a9c5f56235e511683b0557f264a26ec39928
Stanley <38700532+varnerstanley@users.noreply.github.com>
2019-10-19 11:11:43 -0600

    CRM

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+import routes from './src/routes/crmRoutes';
+
+const app = express();
+const PORT = 3000;
+
+  // mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/crmdb', {
+  // useMongoClient: true
+  useNewUrlParser: true
+});
+
+// bodyParser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
+
+// serving static files
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.send(`Node and express server is running on port ${PORT}`)
+);
+
+app.listen(PORT, () =>
+  console.log(`Your server is running on port ${PORT}`)
+);
